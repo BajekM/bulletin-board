c757348d3c509eb908f209468901dac23ae0764d
Marcin Bajek <bajekmarcin@gmail.com>
2020-03-28 09:44:39 +0100

    Repair tests

--- src/components/views/Homepage/Homepage.test.js
+++ src/components/views/Homepage/Homepage.test.js
@@ -1,11 +1,25 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import { HomepageComponent } from './Homepage';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { HomepageComponent } from './Homepage';
+
+const mockProps = {
+  user: 'logged',
+  posts: [
+    {
+      id: 1,
+      title: 'title1',
+    },
+
+    {
+      id: 2,
+      title: 'title2',
+    },
+  ],
+};
 
 describe('Component Homepage',  () => {
   it('should render without crashing',  () => {
-    // const component = await shallow(<HomepageComponent />);
-    const infos = true;
-    expect(infos).toBeTruthy();
+    const component = shallow(<HomepageComponent {...mockProps}/>);
+    expect(component).toBeTruthy();
   });
 });
--- src/components/views/Post/Post.test.js
+++ src/components/views/Post/Post.test.js
@@ -1,11 +1,39 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import { PostComponent } from './Post';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { PostComponent } from './Post';
+
 
 describe('Component Post', () => {
+  const mockProps = {
+    user: 'logged author',
+    match: {
+      params: {
+      },
+    },
+    posts: [
+      {
+        id: 1,
+        title: 'title1',
+        date: 'date1',
+        author: 'author1',
+        content: 'content1',
+      },
+
+      {
+        id: 2,
+        title: 'title2',
+        date: 'date2',
+        author: 'author2',
+        content: 'content2',
+      },
+    ],
+
+  };
+
+  mockProps.match.params.id = 1;
+  
   it('should render without crashing', () => {
-    // const component =  shallow(<PostComponent />);
-    const infos = true;
-    expect(infos).toBeTruthy();
+    const component =  shallow(<PostComponent  {...mockProps}/>);
+    expect(component).toBeTruthy();
   });
 });
